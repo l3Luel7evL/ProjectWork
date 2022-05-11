eb6a728e15efca01fe49782a9ea6d0aded4141e8
Krist1997 <krist.kraja@gmail.com>
2022-05-11 11:32:07 +0200

    Controllo data academy

--- src/main/java/com/energent/service/AcademyService.java
+++ src/main/java/com/energent/service/AcademyService.java
@@ -7,7 +7,7 @@ import com.energent.entity.Academy;
 public interface AcademyService {
 
 	Academy findAcademyById(String id);
-	void addOrUpdateAcademy(Academy academy);
+	void addOrUpdateAcademy(Academy academy) throws Exception;
 	void removeAcademy(String id);
 	List<Academy> findAllAcademies();
 	
