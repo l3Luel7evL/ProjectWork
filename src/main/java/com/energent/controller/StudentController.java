704fe3930c3e33f3cf3c39735f713f8330cc6bb9
UTENTE <UTENTE@192.168.102.239>
2022-05-12 14:40:20 +0200

    Sistemato metodo di add per student tramite navbar access In attesa di ulteriori problemi, da riscontrare ancora, per trovarne soluzione. Matteo.

--- src/main/java/com/energent/controller/StudentController.java
+++ src/main/java/com/energent/controller/StudentController.java
@@ -80,87 +80,15 @@ public class StudentController {
 	public ModelAndView showAllStudents() {return new ModelAndView("studentsAll", "students", studentService.findAllStudents());}
 	
 	@PostMapping("/relate/students")
-	public String relateStudent(@ModelAttribute("student") Student student) throws Exception{
+	public String relateStudent(@ModelAttribute("student") Student student) throws Exception{	
 		List<Academy> academies = student.getAcademies();
 		
-//		student.getAcademies().clear();
-		//student.getAcademies().addAll(academies);
-		
-		for (Academy a : student.getAcademies()) {
-//			a.getStudents().remove(student);
-//			student.getAcademies().remove(a);
-			a.getStudents().add(student);
-			academyService.addOrUpdateAcademy(a);
-			
-			
-		}
-//		student.getAcademies().addAll(academies);
-		studentService.addOrUpdateStudent(student);
-		
-			
-		//student.getAcademies().addAll(academies);
 		for(Academy a : academyService.findAllAcademies())
-			if(!student.getAcademies().contains(a))
+			if(a.getStudents().contains(student) || (!student.getAcademies().contains(a)))
 				studentService.deleteStudentByFiscalCode(student.getFiscalCode(), a.getCode());
 		
-		student.getAcademies().clear();
 		for(Academy a : academies)
 			studentService.insertJoin(a.getCode(), student.getFiscalCode());
-		/*List<Academy> academies = student.getAcademies();
-		
-		student.getAcademies().clear();
-		
-		for(Academy a : academyService.findAllAcademies())
-			if(!academies.contains(a))
-				studentService.deleteStudentByFiscalCode(student.getFiscalCode(), a.getCode());
-		
-		student.getAcademies().addAll(academies);
-		studentService.addOrUpdateStudent(student);
-		
-		for(Academy a : student.getAcademies()) 
-			academyService.addOrUpdateAcademy(a);
-			
-		for(Academy a : academyService.findAllAcademies())
-			if(academies.contains(a))
-				studentService.insertJoin(a.getCode(), student.getFiscalCode());*/
-		
-//		student.getAcademies().clear();
-//		studentService.addOrUpdateStudent(student);
-//		
-////		for(Academy a : academies) {
-////			a.getStudents().remove(student);
-////			academyService.addOrUpdateAcademy(a);
-////		}
-//		
-//		for(Academy a : academyService.findAllAcademies()) {
-//			if(!academies.contains(a)) {
-//				studentService.deleteStudentByFiscalCode(student.getFiscalCode(), a.getCode());
-//				academyService.addOrUpdateAcademy(a);
-//			}
-//		}
-//		student.getAcademies().addAll(academies);
-//		studentService.addOrUpdateStudent(student);
-//		
-//		for(Academy a : academies) {
-//			studentService.insertJoin(a.getCode(), student.getFiscalCode());
-//			academyService.addOrUpdateAcademy(a);
-//			
-//		}
-		
-		
-////		
-////		for(Academy a : academies) {
-////			a.getStudents().add(student);
-////			academyService.addOrUpdateAcademy(a);
-////			
-////		}
-		
-//		
-		
-//		}
-		
-		//for(Academy a : academies)
-		
 		
 		return "redirect:/academies";
 	}
@@ -175,41 +103,3 @@ public class StudentController {
 		return mav;
 	}
 }
-
-/*	//List<Student> students = new ArrayList<>();
-List<Academy> academies = new ArrayList<>();
-
-//students.add(student);
-academies.addAll(student.getAcademies());
-
-/*
-student.getAcademies().clear();
-for(Academy a : academies) {
-	studentService.deleteStudentByFiscalCode(a.getCode(), student.getFiscalCode());
-	a.getStudents().clear();
-}
-
-
-student.getAcademies().addAll(academies);
-
-for(Academy a : academies) {
-	a.getStudents().addAll(students);
-	academyService.addOrUpdateAcademy(a);
-	
-}
-studentService.addOrUpdateStudent(student);
-//student.getAcademies().clear();
-for(Academy a : academies) {
-	a.getStudents().clear();
-	studentService.deleteStudentByFiscalCode(a.getCode(), student.getFiscalCode());
-}
-
-//student.getAcademies().clear();
-student.getAcademies().addAll(academies);
-for(Academy a : academies) {
-	//studentService.deleteStudentByFiscalCode(a.getCode(), student.getFiscalCode());
-	a.getStudents().add(student);
-	academyService.addOrUpdateAcademy(a);
-	
-}
-studentService.addOrUpdateStudent(student);*/
